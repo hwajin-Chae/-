552404d23173d79d8a9da47281af9ac84f7a85f4
avery.Chae <chaehj91@gmail.com>
2022-12-22 18:32:26 +0900

    마켓제보 및 영상 업로드 완료

---
+++ main.js
@@ -0,0 +1,14 @@
+new Swiper('main .market_submit .slide_left .swiper', {
+  // Optional parameters
+  direction: 'vertical',  // 수직 슬라이드
+  loop: true,  // 반복 재생 여부
+  autoplay: true,  // 자동 재생 여부
+});
+
+
+// new Swiper('main .market_submit .slide_left .swiper', {
+//   // Optional parameters
+//   direction: 'vertical',  // 수직 슬라이드
+//   loop: true,  // 반복 재생 여부
+//   autoplay: true,  // 자동 재생 여부
+// });
