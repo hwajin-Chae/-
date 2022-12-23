272edd445975c7cd13ce041ccd15e0047fa4dfb1
avery.Chae <chaehj91@gmail.com>
2022-12-23 18:26:49 +0900

    sub-menu/section 3 작업 중
    1. back ground color 재설정
    2. swiper (slide bar) 1~4 완료

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+new Swiper('main .market_submit .slide_left .swiper', {
+  // Optional parameters
+  direction: 'vertical',  // 수직 슬라이드
+  loop: true,  // 반복 재생 여부
+  autoplay: true,  // 자동 재생 여부
+});
+
+new Swiper('main .market_submit .slide_right .swiper', {
+  // Optional parameters
+  direction: 'vertical',  // 수직 슬라이드
+  loop: true,  // 반복 재생 여부
+  autoplay: true,  // 자동 재생 여부
+});
+
+new Swiper('main .local_star_market .slide_left .swiper', {
+  // Optional parameters
+  direction: 'vertical',  // 수직 슬라이드
+  loop: true,  // 반복 재생 여부
+  autoplay: true,  // 자동 재생 여부
+});
+
+new Swiper('main .local_star_market .slide_right .swiper', {
+  // Optional parameters
+  direction: 'vertical',  // 수직 슬라이드
+  loop: true,  // 반복 재생 여부
+  autoplay: true,  // 자동 재생 여부
+});
