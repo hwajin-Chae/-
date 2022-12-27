48bcade11a0ffb79a2afea552b0203bdd47c8a7a
avery.Chae <chaehj91@gmail.com>
2022-12-27 18:26:52 +0900

    20221227

--- JS/main.js
+++ JS/main.js
@@ -1,64 +1,59 @@
 new Swiper('main .market_submit .slide_left .swiper', {
-  // Optional parameters
-  direction: 'vertical',  // 수직 슬라이드
-  loop: true,  // 반복 재생 여부
-  autoplay: true,  // 자동 재생 여부
+  direction: 'vertical',
+  loop: true,
+  autoplay: true,
 });
 
 new Swiper('main .market_submit .slide_right .swiper', {
-  // Optional parameters
-  direction: 'vertical',  // 수직 슬라이드
-  loop: true,  // 반복 재생 여부
-  autoplay: true,  // 자동 재생 여부
+  direction: 'vertical',
+  loop: true,
+  autoplay: true,
 });
 
 new Swiper('main .local_star_market .slide_left .swiper', {
-  // Optional parameters
-  direction: 'vertical',  // 수직 슬라이드
-  loop: true,  // 반복 재생 여부
-  autoplay: true,  // 자동 재생 여부
+  direction: 'vertical',
+  loop: true,
+  autoplay: true,
 });
 
 new Swiper('main .local_star_market .slide_right .swiper', {
-  // Optional parameters
-  direction: 'vertical',  // 수직 슬라이드
-  loop: true,  // 반복 재생 여부
-  autoplay: true,  // 자동 재생 여부
+  direction: 'vertical',
+  loop: true,
+  autoplay: true,
 });
 
 
 const headerEl = document.querySelector('header');
-    const mainMenuEl = document.querySelector('nav.gnb > .main-menu');
-    const subMenuEls = document.querySelectorAll('nav.gnb .sub-menu');
+  const mainMenuEl = document.querySelector('nav.gnb > .main-menu');
+  const subMenuEls = document.querySelectorAll('nav.gnb .sub-menu');
 
-    mainMenuEl.addEventListener('mouseover', function() {
-      headerEl.style.height = '320px';
+  mainMenuEl.addEventListener('mouseover', function() {
+  headerEl.style.height = '350px';
 
-      subMenuEls.forEach(function (subMenuEl) {
-        subMenuEl.style.opacity = '1';
-        subMenuEl.style.visibility = 'visible';
-      });
+  subMenuEls.forEach(function (subMenuEl) {
+    subMenuEl.style.opacity = '1';
+    subMenuEl.style.visibility = 'visible';
     });
+  });
 
+  headerEl.addEventListener('mouseleave', function () {
+    headerEl.style.height = '165px';
 
-    // mouseout은 자식요소에서 적용되지 않고, 반드시 헤더!! 안!!!에만 있어야 이벤트로 인식
-    // mouseleave는 자식요소에서도 적용되어서, 마우스가 헤더+메뉴에서 벗어나야 이벤트로 인식
-    // mouseover와 mouseebter / mouseout과 mouseleave의 차이점 (w3schools 예제 참고)
-    headerEl.addEventListener('mouseleave', function () {
-      headerEl.style.height = '100px';
-
-      subMenuEls.forEach(function (subMenuEl) {
-        subMenuEl.style.opacity = '0';
-        subMenuEl.style.visibility = 'visible';
-      });
-    })
-
-
-    new Swiper('main .local_star_market .star_market .swiper', {
-      // Optional parameters
-      direction: 'horizontal',  // 수평 슬라이드
-      loop: true,  // 반복 재생 여부
-      autoplay: true,  // 자동 재생 여부
-      slidesPerView: 3.5,
-      spaceBetween: 20,
+    subMenuEls.forEach(function (subMenuEl) {
+      subMenuEl.style.opacity = '0';
+      subMenuEl.style.visibility = 'visible';
     });
+  })
+
+
+new Swiper('main .local_star_market .star_market .swiper', {
+  
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  slidesPerView: 3.5,
+  spaceBetween: 20,
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
